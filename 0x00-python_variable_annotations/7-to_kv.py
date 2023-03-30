0df1145205d07aee7365ca852dff3c1f4d54c366
erickadikah <erickadikah2030@gmail.com>
2023-03-30 18:40:51 +0300

    returns a tuple

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -3,6 +3,6 @@
 from typing import Union, Tuple
 
 
-def to_kv(k: str, v: Union[int, float]) -> tuple[str, float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """takes str and int or float, returns str, square of v as float"""
     return k, v**2.0
