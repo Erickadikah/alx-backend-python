c3f4e5374a62a1e2a108c347f9c1cba7d5f23fc3
erickadikah <erickadikah2030@gmail.com>
2023-03-31 23:22:24 +0300

    advnace task 100 fix

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 """ The types of the elements of the input are not know"""
-from typing import List, Union
+from typing import Sequence, Union, Any
 
 
-def safe_first_element(lst: Union[List, str]) -> Union[str, None]:
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    """args of lst: Sequence[Any]
+        return value: 0 or None
+    """
     if lst:
         return lst[0]
     else:
