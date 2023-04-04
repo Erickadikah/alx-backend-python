890135c80441e42f0b9fc60f03ae5ad3fb965b76
erickadikah <erickadikah2030@gmail.com>
2023-04-04 15:27:07 +0300

    calculating execution time

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""Couroutine executes async_comprehension
+    fourTimes
+"""
+import asyncio
+import time
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """Executing four times in parallel
+        using asyncio.gather and measures the time of execution
+        and returns the total execution time
+    """
+    my_list = []
+    start_time = time.perf_counter()
+    for i in range(4):
+        task = asyncio.create_task(async_comprehension())
+        my_list.append(task)
+    result = await asyncio.gather(*my_list)
+    total_time = time.perf_counter() - start_time
+    return total_time
