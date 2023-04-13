916bbe0b0f73a0857b875c3c2a331ddb60b6a22d
erickadikah <erickadikah2030@gmail.com>
2023-04-13 22:05:44 +0300

    fix

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -6,7 +6,9 @@ from typing import Any, TypeVar, Mapping, Union
 
 T = TypeVar('T')
 
-def safely_get_value(dct: Mapping, key: Any, default: Union[T, None] =None) -> Union[Any, T]:
+
+def safely_get_value(dct: Mapping, key: Any,
+                     default: Union[T, None] = None) -> Union[Any, T]:
     """ T = defining a type variable
         to specify the variable.
         T = union of type and any
