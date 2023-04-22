3213360fb306d2c1d21e18e5ff33fd06c9f2aa0f
erickadikah <erickadikah2030@gmail.com>
2023-04-22 18:45:29 +0300

    test

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -3,7 +3,11 @@
 """
 import unittest
 from parameterized import parameterized
-from utils import access_nested_map
+from utils import access_nested_map, memoize
+import requests
+from unittest import mock
+import functools
+
 
 class TestAccessNestedMap(unittest.TestCase):
     """
@@ -30,3 +34,49 @@ class TestAccessNestedMap(unittest.TestCase):
         """
         with self.assertRaises(Exception):
             access_nested_map(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    """Method to test utils.get_json return expected output
+        using mock.patch to patch requests.get
+        Reaturns: a mock object
+    """
+
+    def test_get_json(self):
+        """
+        Test that the utils.get_json function returns the expected output.
+        Returns: test_payload = {'key': 'value'}
+        test_url = "http://example.com"
+        response = requests.get(test_url)
+        return response.json()
+        """
+        test_payload = {'key': 'value'}
+        with mock.patch('requests.get') as mock_get:
+            mock_get.return_value.json.return_value = test_payload
+            response = requests.get('http://example.com')
+            data = response.json()
+            self.assertEqual(data, test_payload)
+
+
+class TestMemoize(unittest.TestCase):
+    """Tsting if function memoize is being called one
+    """
+
+    def memoize(func):
+        """memoize func
+        """
+        return functools.lru_cache()(func)
+
+    def test_memoize(self):
+        """test
+        """
+        class TestClass:
+            """Class test for the return value 42
+            """
+
+            def a_method(self):
+                return 42
+
+        @memoize
+        def a_property(self, arg):
+            return self.a_method() + arg
